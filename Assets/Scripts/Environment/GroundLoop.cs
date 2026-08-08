3876a036d86168cfadbb6a747a04ff919a8f4f22
DryerDock <62032426+DryerDock@users.noreply.github.com>
2026-08-08 22:11:57 +0100

    Pre-set up
    Running animation, floor movement, pre-set up complete
    moving onto UI controller

---
+++ Assets/Scripts/Environment/GroundLoop.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class GroundLoop : MonoBehaviour
+{
+    public float resetX;
+    public float moveToX;
+
+    private void Update()
+    {
+        if (transform.position.x < resetX)
+        {
+            transform.position = new Vector3(moveToX, transform.position.y, transform.position.z);
+        }
+    }
+}
